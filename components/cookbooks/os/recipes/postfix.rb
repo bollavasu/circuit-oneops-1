640087980abd7e747cfb10247fc11d467a2cc31d
bollavasu <bolla.vasu@gmail.com>
2017-04-21 17:38:11 +0530

    Update postfix.rb

--- components/cookbooks/os/recipes/postfix.rb
+++ components/cookbooks/os/recipes/postfix.rb
@@ -1,4 +1,6 @@
 if node.platform != "ubuntu"
+ if(node[:workorder][:cloud][:ciAttributes][:location].index('google') == nil)
   execute "sudo pkill -f '^/usr/sbin/postfix -d' ; service postfix restart"
+ end
 end
   
